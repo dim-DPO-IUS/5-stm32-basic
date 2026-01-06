658c454b34d3b8cc26d9018757c50d39aab7c8a5
dimakonoplev <work4dak@mail.ru>
2026-01-06 21:52:40 +0300

    base4: display ST7789 set hard

--- Course/Core/Inc/main.h
+++ Course/Core/Inc/main.h
@@ -61,10 +61,14 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define TFT_RESET_Pin GPIO_PIN_1
+#define TFT_RESET_GPIO_Port GPIOA
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define TFT_DC_Pin GPIO_PIN_4
+#define TFT_DC_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
